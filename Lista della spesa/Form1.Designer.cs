68892b052d176121ae6d4ac0f8f6f0f389557997
Alessandro <12999548@itisrossi.vi.it>
2025-03-28 16:47:27 +0100

    Aggiungere i file di progetto.

---
+++ Lista della spesa/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace Lista_della_spesa
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pnl_BottoniAlti = new Panel();
+            txt_QuantitaProdotto = new TextBox();
+            txt_CostoProdotto = new TextBox();
+            txt_NomeProdotto = new TextBox();
+            btn_Rimuovi = new Button();
+            btn_Aggiungi = new Button();
+            lbl_TitoloBottoniAlti = new Label();
+            lst_ListaDellaSpesa = new ListBox();
+            pnl_BottoniAlti.SuspendLayout();
+            SuspendLayout();
+            // 
+            // pnl_BottoniAlti
+            // 
+            pnl_BottoniAlti.Controls.Add(txt_QuantitaProdotto);
+            pnl_BottoniAlti.Controls.Add(txt_CostoProdotto);
+            pnl_BottoniAlti.Controls.Add(txt_NomeProdotto);
+            pnl_BottoniAlti.Controls.Add(btn_Rimuovi);
+            pnl_BottoniAlti.Controls.Add(btn_Aggiungi);
+            pnl_BottoniAlti.Location = new Point(12, 37);
+            pnl_BottoniAlti.Name = "pnl_BottoniAlti";
+            pnl_BottoniAlti.Size = new Size(776, 104);
+            pnl_BottoniAlti.TabIndex = 0;
+            // 
+            // txt_QuantitaProdotto
+            // 
+            txt_QuantitaProdotto.Font = new Font("Calibri", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            txt_QuantitaProdotto.Location = new Point(322, 43);
+            txt_QuantitaProdotto.Name = "txt_QuantitaProdotto";
+            txt_QuantitaProdotto.PlaceholderText = "Quantità prodotto";
+            txt_QuantitaProdotto.Size = new Size(125, 22);
+            txt_QuantitaProdotto.TabIndex = 1;
+            // 
+            // txt_CostoProdotto
+            // 
+            txt_CostoProdotto.Font = new Font("Calibri", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            txt_CostoProdotto.Location = new Point(173, 43);
+            txt_CostoProdotto.Name = "txt_CostoProdotto";
+            txt_CostoProdotto.PlaceholderText = "Costo prodotto";
+            txt_CostoProdotto.Size = new Size(125, 22);
+            txt_CostoProdotto.TabIndex = 1;
+            // 
+            // txt_NomeProdotto
+            // 
+            txt_NomeProdotto.Font = new Font("Calibri", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            txt_NomeProdotto.Location = new Point(22, 43);
+            txt_NomeProdotto.Name = "txt_NomeProdotto";
+            txt_NomeProdotto.PlaceholderText = "Nome prodotto";
+            txt_NomeProdotto.Size = new Size(125, 22);
+            txt_NomeProdotto.TabIndex = 1;
+            // 
+            // btn_Rimuovi
+            // 
+            btn_Rimuovi.Font = new Font("Calibri", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            btn_Rimuovi.Location = new Point(651, 33);
+            btn_Rimuovi.Name = "btn_Rimuovi";
+            btn_Rimuovi.Size = new Size(103, 39);
+            btn_Rimuovi.TabIndex = 0;
+            btn_Rimuovi.Text = "RIMUOVI";
+            btn_Rimuovi.UseVisualStyleBackColor = true;
+            btn_Rimuovi.Click += btn_Rimuovi_Click;
+            // 
+            // btn_Aggiungi
+            // 
+            btn_Aggiungi.Font = new Font("Calibri", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            btn_Aggiungi.Location = new Point(513, 32);
+            btn_Aggiungi.Name = "btn_Aggiungi";
+            btn_Aggiungi.Size = new Size(103, 39);
+            btn_Aggiungi.TabIndex = 0;
+            btn_Aggiungi.Text = "AGGIUNGI";
+            btn_Aggiungi.UseVisualStyleBackColor = true;
+            btn_Aggiungi.Click += btn_Aggiungi_Click;
+            // 
+            // lbl_TitoloBottoniAlti
+            // 
+            lbl_TitoloBottoniAlti.AutoSize = true;
+            lbl_TitoloBottoniAlti.Font = new Font("Calibri", 11F, FontStyle.Regular, GraphicsUnit.Point);
+            lbl_TitoloBottoniAlti.Location = new Point(12, 19);
+            lbl_TitoloBottoniAlti.Name = "lbl_TitoloBottoniAlti";
+            lbl_TitoloBottoniAlti.Size = new Size(152, 18);
+            lbl_TitoloBottoniAlti.TabIndex = 1;
+            lbl_TitoloBottoniAlti.Text = "Seleziona una funzione";
+            // 
+            // lst_ListaDellaSpesa
+            // 
+            lst_ListaDellaSpesa.FormattingEnabled = true;
+            lst_ListaDellaSpesa.ItemHeight = 15;
+            lst_ListaDellaSpesa.Location = new Point(12, 147);
+            lst_ListaDellaSpesa.Name = "lst_ListaDellaSpesa";
+            lst_ListaDellaSpesa.Size = new Size(776, 274);
+            lst_ListaDellaSpesa.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(lst_ListaDellaSpesa);
+            Controls.Add(lbl_TitoloBottoniAlti);
+            Controls.Add(pnl_BottoniAlti);
+            Name = "Form1";
+            Text = "Lista della spesa";
+            Load += Form1_Load;
+            pnl_BottoniAlti.ResumeLayout(false);
+            pnl_BottoniAlti.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel pnl_BottoniAlti;
+        private Button btn_Aggiungi;
+        private Label lbl_TitoloBottoniAlti;
+        private TextBox txt_QuantitaProdotto;
+        private TextBox txt_CostoProdotto;
+        private TextBox txt_NomeProdotto;
+        private Button btn_Rimuovi;
+        private ListBox lst_ListaDellaSpesa;
+    }
+}
---
+++ Lista della spesa/Form1.cs
@@ -0,0 +1,100 @@
+using System.Runtime.CompilerServices;
+
+namespace Lista_della_spesa
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string[] Lista_della_spesa = new string[20];
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            caricaNellaLista();
+
+        }
+
+        private void btn_Aggiungi_Click(object sender, EventArgs e)
+        {
+            nuovoElemento();
+        }
+        private void btn_Rimuovi_Click(object sender, EventArgs e)
+        {
+            rimuoviElemento();
+        }
+
+        private void caricaNellaLista()
+        {
+            string riga;
+            string[] lineaTotale;
+            int i = 0;
+            // appena si carica il form io vado a leggermi i dati dal file e successivamente caricarli nella lista
+            using (StreamReader rd = new StreamReader("lista.csv"))
+            {
+                while (!rd.EndOfStream)
+                {
+                    riga = rd.ReadLine();
+                    Lista_della_spesa[i] = riga;
+                    // splitto
+                    lineaTotale = riga.Split(";");
+                    // carico la riga nella lista
+                    string itemFormatted = $"{lineaTotale[0].PadRight(15)} {lineaTotale[1].PadRight(15)} {lineaTotale[2]}";
+                    lst_ListaDellaSpesa.Items.Add(itemFormatted);
+                    i++;
+                }
+            }
+        }
+
+        private void nuovoElemento()
+        {
+            if (string.IsNullOrWhiteSpace(txt_NomeProdotto.Text) ||
+                int.IsNegative(int.Parse(txt_CostoProdotto.Text)) ||
+                int.IsNegative(int.Parse(txt_QuantitaProdotto.Text)))
+            {
+                MessageBox.Show("Inserisci dei valori validi!", "ERRORE!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txt_NomeProdotto.Text = "";
+                txt_CostoProdotto.Text = "";
+                txt_QuantitaProdotto.Text = "";
+                return;
+            }
+            // qua l'elemento è valido quindi
+            using (StreamWriter wr = new StreamWriter("lista.csv"))
+            {
+                wr.WriteLine($"{txt_NomeProdotto.Text};{(int.Parse(txt_CostoProdotto.Text))};{(int.Parse(txt_QuantitaProdotto.Text))}");
+            }
+            caricaNellaLista();
+        }
+
+        private void rimuoviElemento()
+        {
+            if (lst_ListaDellaSpesa.SelectedItems.Count != 1)
+            {
+                MessageBox.Show("Seleziona solo un elemento!", "ERRORE!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            using (StreamReader reader = new StreamReader("lista.csv"))
+            using (StreamWriter wr = new StreamWriter("lista_temp.csv"))
+            {
+                int currentLineIndex = 0;
+                string line;
+
+                // Scorri tutte le righe del file
+                while ((line = reader.ReadLine()) != null)
+                {
+                    // Se non è la riga selezionata, scrivila nel nuovo file
+                    if (currentLineIndex != lst_ListaDellaSpesa.SelectedIndex)
+                    {
+                        wr.WriteLine(line);
+                    }
+
+                    currentLineIndex++;
+                }
+            }
+            File.Delete("lista.csv");
+            File.Move("lista_temp.csv", "lista.csv");
+            lst_ListaDellaSpesa.Items.Clear();
+            caricaNellaLista();
+        }
+    }
+}
